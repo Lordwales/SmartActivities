600ebdf99eba08723d3490817ce2670400b374a4
OLAWALE TOFADE <tofadeolawale@gmail.com>
2020-05-10 21:44:19 +0100

    Added Photo upload and user Profile

--- Application/User/CurrentUser.cs
+++ Application/User/CurrentUser.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Interfaces;
@@ -35,7 +36,7 @@ namespace Application.User
                        DisplayName = user.DisplayName,
                        Token = _jwtGenerator.CreateToken(user),
                        Username = user.UserName,
-                       Image = null
+                       Image = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
                    };
             }
         }
