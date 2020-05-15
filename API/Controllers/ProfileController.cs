0485562f80c052f36f6fb8bee31f842d2a209d04
OLAWALE TOFADE <tofadeolawale@gmail.com>
2020-05-15 01:07:45 +0100

    Added, paging filtering & sorting functionalities

--- API/Controllers/ProfileController.cs
+++ API/Controllers/ProfileController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application.Profiles;
 using MediatR;
@@ -18,6 +19,13 @@ namespace API.Controllers
         {
             return await Mediator.Send(command);
         }
+
+        [HttpGet ("{username}/activities")]
+        public async Task <ActionResult<List<UserActivityDto>>> GetUserActivities(string username, string predicate)
+        {
+            return await Mediator.Send(new ListActivities.Query{Username = username, Predicate = predicate});
+        }
+
     }
 
 
